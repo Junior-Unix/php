b2337d266ea5f5677f80b98ca9266376a53d15e2
Junior-Unix <sonjunior@live.com>
2021-09-25 19:57:12 -0300

    PDO criar bd  sqlServer.

---
+++ PDO/dbphp7SQLServer.sql
@@ -0,0 +1,11 @@
+/* CREATE TABLE tb_usuarios(
+    idusuarios INT NOT NULL IDENTITY PRIMARY KEY,
+    deslogin VARCHAR(64) NOT NULL,
+    dessenha VARCHAR(256) NOT NULL,
+    dtcadastro DATETIME NOT NULL DEFAULT GETDATE()
+); */
+
+/* SELECT * FROM tb_usuarios; */
+
+/* INSERT INTO tb_usuarios (deslogin, dessenha) VALUES ('Junior-Unix', 'password'); */
+
