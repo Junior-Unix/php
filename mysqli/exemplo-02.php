72b420b0c510a3939d7a7956e3f67daceb7e2ef3
Junior-Unix <sonjunior@live.com>
2021-09-25 19:08:47 -0300

    mysqli SELECT * FROM array_push, json_encode. o anterior estava errado.

--- mysqli/exemplo-02.php
+++ mysqli/exemplo-02.php
@@ -9,8 +9,8 @@ $result = $conn->query("SELECT * FROM tb_usuarios ORDER BY deslogin");
 
     $data = array();
 
-    while($row = $result->fetch_array(MYSQLI_ASSOC)){
-        array_push($data = $row);
+    while($row = $result->fetch_assoc()){
+        array_push($data, $row);
     }
 
     echo json_encode($data);
