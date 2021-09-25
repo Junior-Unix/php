7ca8b61dd643f16ff2c07d888f9bd16fdaea07cf
Junior-Unix <sonjunior@live.com>
2021-09-25 19:05:19 -0300

    mysqli select * from. MYSQL_ASSOC.

--- mysqli/exemplo-02.php
+++ mysqli/exemplo-02.php
@@ -6,6 +6,6 @@ $conn = new mysqli("localhost", "root", "0008", "dbphp7");
     }
 
 $result = $conn->query("SELECT * FROM tb_usuarios ORDER BY deslogin");
-    while($row = $result->fetch_array()){
+    while($row = $result->fetch_array(MYSQLI_ASSOC)){
         var_dump($row);
     }
