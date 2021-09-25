7f8443efa80b1717be9592e88d5eb10a6c1aa975
Junior-Unix <sonjunior@live.com>
2021-09-25 19:23:59 -0300

    PDO connexão, foreach, fetchAll.

--- PDO/exemplo-01.php
+++ PDO/exemplo-01.php
@@ -6,6 +6,12 @@ $stmt = $conn->prepare("SELECT * FROM tb_usuarios ORDER BY deslogin");
 
 $stmt->execute();
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
-var_dump($results);
+
+foreach($results as $row){
+    foreach($row as $key => $value){
+        echo "<strong>".$key.":</strong>".$value."<br/>";
+    }
+    echo "===========================================<br>";
+}
 
 ?>
