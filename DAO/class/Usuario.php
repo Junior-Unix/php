246d9ee739a13b02029e38eda59fbbf95d6c34c7
Junior-Unix <sonjunior@live.com>
2021-09-29 12:25:37 -0300

    Testes.

--- DAO/class/Usuario.php
+++ DAO/class/Usuario.php
@@ -59,6 +59,23 @@ class Usuario{
 		return $sql->select("SELECT * FROM tb_usuarios WHERE deslogin LIKE :SEARCH ORDER BY deslogin", array(':SEARCH'=>"%".$login."%"));
 	}
 
+	public function login($login, $password){
+		$sql = new Sql();
+         $results = $sql->select("SELECT * FROM tb_usuarios WHERE deslogin =  :LOGIN AND dessenha = :PASSWORD", array(":LOGIN"=>$login, ":PASSWORD"=>$password));
+
+			if(isset($results[0])){ //ou count no lugar do isset
+				$row = $results[0];
+
+				$this->setIdusuario($row['idusuario']);
+				$this->setDeslogin($row['deslogin']);
+				$this->setDessenha($row['dessenha']);
+				$this->setDtcadastro(new DateTime($row['dtcadastro']));
+			}else{
+				throw new Exception("Login e/ou senha inválidos.");
+			}
+	}
+
+	
 	public function __toString(){
 
 		return json_encode(array(
--- DAO/index.php
+++ DAO/index.php
@@ -13,18 +13,15 @@ echo json_encode($usuarios);
 $root->loadById(1);
 echo $root; */
 
-/* //Carrega uma lista de usuários
-$lista = Usuario::getList();
+//Carrega uma lista de usuários
+/* $lista = Usuario::getList();
 echo json_encode($lista); */
 
 //Carrega uma lista de usuários buscando pelo login
-$search = Usuario::search("Junior");
-echo json_encode($search);
+/* $search = Usuario::search("Y");
+echo json_encode($search); */
 
 //Carrega um usuário usando o login e a senha
-/* $usuario = new Usuario();
-$usuario->login("root", "0008");
 
-echo $usuario; */
 
 ?>
