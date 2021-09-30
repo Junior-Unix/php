2bb736b322eace00549bf9cd4296f852109b841b
Junior-Unix <sonjunior@live.com>
2021-09-30 12:28:06 -0300

    Del ~.

--- FGETS/exemplo-02.php
+++ FGETS/exemplo-02.php
@@ -8,13 +8,11 @@ $fileinfo = new finfo(FILEINFO_MIME_TYPE);
 
 $mimetype = $fileinfo->file($filename);
 
-echo "data:" . $mimetype . ";base64," . $base64;
+$base64encode =  "data:" . $mimetype . ";base64," . $base64;
 
 
 ?>
-
-
 	<a href="<?=$base64encode?>" target="_blank">Link Para Imagem</a?>
-	<img src=<"<?=$base64encode?>">
+	<img src="<?=$base64encode?>">
 
 ?>
