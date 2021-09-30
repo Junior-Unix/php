65748b52edc4419b3e2abc454670b181caf6eb89
Junior-Unix <sonjunior@live.com>
2021-09-30 17:15:54 -0300

    GD bibliotek, certificado, exemplo 02, arquivo em pasta.

--- GD/exemplo-02.php
+++ GD/exemplo-02.php
@@ -5,13 +5,13 @@ $image = imagecreatefromjpeg("certificado.jpg");
 $titleColor = imagecolorallocate($image, 0, 0, 0);
 $gray = imagecolorallocate($image, 100, 100, 100);
 
-imagestring($image, 5, 450, 150, "CERTIFICADO", $titleColor); // fonte, distância lateral, distância do topo
-imagestring($image, 5, 440, 350, "Junior-Unix", $titleColor);
-imagestring($image, 3, 440, 370, utf8_decode("Concluído em ") . date("d-m-Y"), $titleColor); // utf8_encode piorou a gráfia.
+imagestring($image, 5, 400, 150, "CERTIFICADO", $titleColor); // fonte, distância lateral, distância do topo
+imagestring($image, 5, 390, 350, "Junior-Unix", $titleColor);
+imagestring($image, 3, 390, 370, utf8_decode("Concluído em ") . date("d-m-Y"), $titleColor); // utf8_encode piorou a gráfia.
 
 header("Content-type: image/jpeg");
+imagejpeg($image); // em tela.
+imagejpeg($image, "certificado-".date("Y-m-d"). " .jpg"); //em documento.
 
-imagejpeg($image);
-
-imagedestroy();
+imagedestroy($image);
 ?>
