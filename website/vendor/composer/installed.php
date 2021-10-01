27d4ded4e7c9f70cafdcda32223bb23158b2a59b
Junior-Unix <sonjunior@live.com>
2021-10-01 15:01:50 -0300

    Slim.

---
+++ website/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '650730671f738f16750ee89871933816fe5e1e8d',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '650730671f738f16750ee89871933816fe5e1e8d',
+            'dev_requirement' => false,
+        ),
+        'slim/slim' => array(
+            'pretty_version' => '2.0.0',
+            'version' => '2.0.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../slim/slim',
+            'aliases' => array(),
+            'reference' => '3e95c9abbc57a8f023ab4dacebcab9dae4d9f1f0',
+            'dev_requirement' => false,
+        ),
+    ),
+);
